0480dc097233f481d7f6310e2e38783119db0702
sileo18 <lucastoledo358@gmail.com>
2025-06-07 00:23:17 -0300

    fix: dns problem solved

--- WordsAPI/Config/EmailSettings.cs
+++ WordsAPI/Config/EmailSettings.cs
@@ -5,17 +5,16 @@
     {
         public class EmailSettings
         {
-            // Propriedades SMTP (podem ser úteis para fallback, mas não para o cliente SendGrid API)
             public string SmtpHost { get; set; } = string.Empty;
             public int SmtpPort { get; set; }
             public bool EnableSsl { get; set; }
 
             public string SenderName { get; set; } = string.Empty;
             public string SenderEmail { get; set; } = string.Empty;
-            public string Password { get; set; } = string.Empty; // Não mais usada pelo cliente SendGrid API
+            public string Password { get; set; } = string.Empty; 
             public string ReceiverEmail { get; set; } = string.Empty;
 
-            public string SendGridApiKey { get; set; } = string.Empty; // <<<< ESTA PROPRIEDADE
+            public string SendGridApiKey { get; set; } = string.Empty; 
         }
     }
 }
